d92a7e29d502d552153b1e644791db78c0c4ff2d
Shogun <dimitartsvetkov89@gmail.com>
2018-05-26 10:32:33 +0300

    Firebase-integration

--- src/components/registration/Register.js
+++ src/components/registration/Register.js
@@ -6,19 +6,12 @@ import TextField from 'material-ui/TextField';
 import { Link } from 'react-router-dom';
 import {lightGreen700} from 'material-ui/styles/colors';
 import {lightGreen900} from 'material-ui/styles/colors';
-import img from './woodsign.png';
 
 
 
 
-const muiTheme = getMuiTheme({
-    palette: {
-        textColor: lightGreen700,
-    },
-    appBar: {
-        color: lightGreen900
-    }
-});
+
+
 class Register extends Component {
     constructor(props){
         super(props);
@@ -36,54 +29,37 @@ class Register extends Component {
         return <div className="row">
             <div className="col-lg-3"/>
             <div className="col-lg-6">
-                <MuiThemeProvider muiTheme={muiTheme}>
                     <div className="screenreg">
                         <div className="row">
+                            <header className="header">
+                                <h1>Registration</h1>
+                            </header>
                             <div className="col-lg-3"/>
                             <div className="col-lg-6">
-                                <AppBar
-                                    title="Registration"
-                                    showMenuIconButton={false}
-                                />
-                                <TextField
-                                    hintStyle={{color: lightGreen700}}
-                                    hintText="Enter your First Name"
-                                    floatingLabelStyle={{color: lightGreen900}}
-                                    floatingLabelText="First Name"
+                                <input className="inputform"
+                                    placeholder="Enter your First Name"
                                     onChange={(event, newValue) => this.setState({first_name: newValue})}
                                 />
                                 <br/>
-                                <TextField
-                                    hintStyle={{color: lightGreen700}}
-                                    hintText="Enter your Last Name"
-                                    floatingLabelStyle={{color: lightGreen900}}
-                                    floatingLabelText="Last Name"
+                                <input className="inputform"
+                                    placeholder="Enter your Last Name"
                                     onChange={(event, newValue) => this.setState({last_name: newValue})}
                                 />
                                 <br/>
-                                <TextField
-                                    hintStyle={{color: lightGreen700}}
-                                    hintText="Enter your username"
-                                    floatingLabelStyle={{color: lightGreen900}}
-                                    floatingLabelText="Username"
+                                <input className="inputform"
+                                    placeholder="Enter your username"
                                     onChange={(event, newValue) => this.setState({username: newValue})}
                                 />
                                 <br/>
-                                <TextField
-                                    hintStyle={{color: lightGreen700}}
-                                    hintText="Enter your Email"
-                                    floatingLabelStyle={{color: lightGreen900}}
+                                <input className="inputform"
+                                    placeholder="Enter your Email"
                                     type="email"
-                                    floatingLabelText="Email"
                                     onChange={(event, newValue) => this.setState({email: newValue})}
                                 />
                                 <br/>
-                                <TextField
-                                    hintStyle={{color: lightGreen700}}
+                                <input className="inputform"
                                     type="password"
-                                    hintText="Enter your Password"
-                                    floatingLabelStyle={{color: lightGreen900}}
-                                    floatingLabelText="Password"
+                                    placeholder="Enter your Password"
                                     onChange={(event, newValue) => this.setState({password: newValue})}
                                 />
                                 <br/>
@@ -101,7 +77,6 @@ class Register extends Component {
                             <div className="col-lg-3"/>
                         </div>
                     </div>
-                </MuiThemeProvider>
             </div>
             <div className="col-lg-3"/>
         </div>;
